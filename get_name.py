7fb33aac2f7f89c7de6088539deb8a312966d851
Dalai P <148987910+DalaiCB@users.noreply.github.com>
2024-07-02 17:00:55 -0500

    Add files via upload

---
+++ get_name.py
@@ -0,0 +1,31 @@
+import os
+import re
+from icecream import ic
+
+def list_md_files(directory):
+    
+    return [f for f in os.listdir(directory) if f.endswith('.md')]
+
+def normalize_string(s):
+    
+    return re.sub(r'[\s\-_/]', '', s.lower())
+
+def search_product_files(product_name):
+    if "/" in product_name:
+        product_name = product_name.replace("/", "\\")
+
+    md_files = list_md_files('data/test_md')
+    normalized_product_name = normalize_string(product_name)
+    
+    matches = []
+    for md_file in md_files:
+        normalized_md_file = normalize_string(md_file.replace('.md', ''))
+        if normalized_product_name in normalized_md_file:
+            matches.append(md_file)
+    
+    print("Matching files:", matches)
+
+    return matches
+
+if __name__ == "__main__":
+    search_product_files('IGA 320/23')
---
+++ gpt_extract_name.py
@@ -0,0 +1,45 @@
+import os
+from openai import OpenAI
+
+def name_extracter(user_query):
+    client = OpenAI(
+            api_key=os.environ.get("OPENAI_API_KEY"),
+    )
+
+    # System prompt. Query processing instructions for the AI assistant
+    sys_prompt = """
+    You are an assistant that extracts product names from user queries.
+    If the query contains more than one product name, provide all the names separated by double tilde '~~'.
+    Some product names may contain '-', '/', or other special characters. Please include them as well.
+    If no product name is found, return None.
+    """
+
+    prompt = f"Query: {user_query}"
+
+    # Call the OpenAI API to process the user query
+    response = client.chat.completions.create(
+        model="gpt-4o", 
+        messages=[
+            {"role": "system", "content": sys_prompt},
+            {"role": "user", "content": prompt}
+        ], 
+        max_tokens=50
+    )
+
+    stripped_response = response.choices[0].message.content.strip()
+
+    if "~~" in stripped_response:
+        multiple_products = True
+        message_content = stripped_response.split("~~")
+    elif "None" in stripped_response:
+        return None, False
+    else:
+        message_content = [stripped_response]
+        multiple_products = False
+
+    return message_content, multiple_products
+
+if __name__ == "__main__":
+    query = "What are the temperature ranges of IGA6/23?"
+
+    print(name_extracter(query))
