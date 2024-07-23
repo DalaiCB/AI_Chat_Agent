5a2107670a255ce77084a9c3a01d71d231d620b7
Dalai P <148987910+DalaiCB@users.noreply.github.com>
2024-07-23 11:44:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -41,9 +41,12 @@ def dataframe_to_html(df):
 
 
 def format_message(message):
-    # Replace newline characters with HTML line breaks
-    message = message.replace('\n', '<br>')
-    message = message.replace('\t', '&nbsp;&nbsp;&nbsp;&nbsp;')
+    try:
+        # Replace newline characters with HTML line breaks
+        message = message.replace('\n', '<br>')
+        message = message.replace('\t', '&nbsp;&nbsp;&nbsp;&nbsp;')
+    except Exception as e:
+        ic(e)
     return message
 
 
