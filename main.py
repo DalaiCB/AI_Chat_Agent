fe8360491f1753926922af44e0ffec2d52fb9296
Dalai P <148987910+DalaiCB@users.noreply.github.com>
2024-08-13 12:18:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -239,7 +239,8 @@ def main(raw_user_query, session_id):
         print("\n\t\tError: General search failed.\n", e)
         return ERROR_MESSAGE
 
-    print("\n\t\tAgent Response:\n", agent_response)
+    print(" ")
+    print("\nAgent Response:\n", agent_response)
     print("---------------")
     print("\n------END------\n")
     print("---------------")
