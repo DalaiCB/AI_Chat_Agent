00dd19a1b0dacb75f1131c209a713f25b5b970da
Dalai P <148987910+DalaiCB@users.noreply.github.com>
2024-09-02 14:15:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -217,7 +217,7 @@ def main(raw_user_query, session_id):
                 if special_case:
                     return search_special_product(user_query)
             except Exception as e:
-                os.write(1, f"\n\t\tError: Special product search failed.\n {e}")
+                os.write(1, f"\n\t\tError: Special product search failed.\n {e}".encode('utf-8'))
                 
                 return ERROR_MESSAGE
 
@@ -229,7 +229,7 @@ def main(raw_user_query, session_id):
             return recommended_products
 
     except Exception as e:
-        os.write(1, f"\n\t\tError: main()\n {e}")
+        os.write(1, f"\n\t\tError: main()\n {e}".encode('utf-8'))
         return ERROR_MESSAGE
 
     # Regular search for product information
@@ -238,7 +238,7 @@ def main(raw_user_query, session_id):
         agent_response, extracted_product_names = search(user_query, session_id)
         print("\n\n\t\tProduct Names:", extracted_product_names)
     except Exception as e:
-        os.write(1, f"\n\t\tError: General search failed.\n{e}")
+        os.write(1, f"\n\t\tError: General search failed.\n{e}".encode('utf-8'))
         return ERROR_MESSAGE
     
     return agent_response
